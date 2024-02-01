ec113237c7d5142eb6fee061b82786e32cc8c665
scrafi <salmanchowdhuryrafi@gmail.com>
2024-02-02 00:09:58 +0800

    comments added, some delay after signup added

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,28 @@
-const formObj = {username: '', email: '', pass: ''};
+const formObj = {username: '', email: '', pass: ''}; // saving form info as object
 
+// signup form event
 const formElement = document.getElementById('signin-form');
 formElement.addEventListener('submit', (event) => {
     event.preventDefault();
-    const emailElement = document.querySelector('.js-reg-email');
-    const passElement = document.querySelector('.js-reg-pass');
-    const confirmPassElement = document.querySelector('.js-reg-confirm-pass');
-    const warningElement = document.querySelector('.js-warning');
-    const usernameElement = document.querySelector('.js-reg-username');
-    formObj.username = usernameElement.value;
-    if (emailElement) {
+    const emailElement = document.querySelector('.js-reg-email'); // form email
+    const passElement = document.querySelector('.js-reg-pass'); // form pass
+    const confirmPassElement = document.querySelector('.js-reg-confirm-pass'); // form confirm pass
+    const warningElement = document.querySelector('.js-warning'); // form warning
+    const usernameElement = document.querySelector('.js-reg-username'); //form username
+    formObj.username = usernameElement.value; // adding username to the object
+    if (emailElement) { // email availablity condition
         formObj.email = emailElement.value;
     }
-    const getPass = passElement.value;
-    if (confirmPassElement) {
+    const getPass = passElement.value; // adding pass to the object
+    if (confirmPassElement) { // confirm pass availablity condition
         const getConfirmPass = confirmPassElement.value;
     
-        if (getPass === getConfirmPass) {
+        if (getPass === getConfirmPass) { // condition if pass and confirm pass matches
             formObj.pass = getPass;
 
-            warningElement.innerHTML = `Congratulations!! ${formObj.username}, You have successfully registered. Now please sign in.`;
+            warningElement.innerHTML = `Congratulations!! ${formObj.username}, You have successfully registered. Now please sign in.`; // success warning html
             
+            // sign in form html
             const signInHTML = `
             <form id="signin-form">
             <h3 class="css-heading">Sign In</h3>
@@ -32,11 +34,18 @@ formElement.addEventListener('submit', (event) => {
             </div>
             </form>
             `;
-            formElement.innerHTML = signInHTML;
-            formElement.addEventListener('submit', (event) => {
-                event.preventDefault();
-                signInAction();
-            });
+
+            // Delayed code
+            setTimeout(function() {
+                // Code to be executed after the delay
+                formElement.innerHTML = signInHTML;
+                formElement.addEventListener('submit', (event) => {
+                    event.preventDefault();
+                    // executing signin action function
+                    signInAction(usernameElement, passElement, warningElement);
+                });
+            }, 2000);
+  
         }
         else {
             warningElement.innerHTML = '<p class="css-warning">Your password and confirm password doesn\'t match</p>'
@@ -44,14 +53,13 @@ formElement.addEventListener('submit', (event) => {
     }
 });
 
-
-function signInAction() {
-    const signInUserElement = document.querySelector('.js-reg-username');
-    const signinPassElement = document.querySelector('.js-reg-pass');
-    const warningElement = document.querySelector('.js-warning');
+// signin action function
+function signInAction(signInUserElement, signinPassElement, warningElement) {
+    // getting user and pass from html element
     const signInUserName = signInUserElement.value;
     const signInPass = signinPassElement.value;
 
+    // conditions for signin
     if (signInUserName === formObj.username && signInPass === formObj.pass) {
         signInSuccess = `<p class="css-signin">${formObj.username}, You have successfully signed in</p>`;
 
